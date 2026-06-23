8002da62e7f00b3184f68a9d0538f85c1e0fa163
Lanh Hoang <lanhhoang@users.noreply.github.com>
2026-06-23 17:51:14 -0400

    feat: add shared types and constants for plan mode

---
+++ src/shared/constants.ts
@@ -0,0 +1,55 @@
+export const STATE_ENTRY_TYPE = "plan-mode-state";
+export const STATUS_KEY = "pi-plan";
+
+export const SAFE_BUILTIN_PLAN_TOOLS = new Set([
+  "read",
+  "bash",
+  "grep",
+  "find",
+  "ls",
+]);
+export const BLOCKED_BUILTIN_TOOLS = new Set(["edit", "write"]);
+export const DEFAULT_TOOLS = ["read", "bash", "edit", "write"];
+
+export const MUTATING_BASH_PATTERNS: RegExp[] = [
+  /\brm\b/i,
+  /\brmdir\b/i,
+  /\bmv\b/i,
+  /\bcp\b/i,
+  /\bmkdir\b/i,
+  /\btouch\b/i,
+  /\bchmod\b/i,
+  /\bchown\b/i,
+  /\bchgrp\b/i,
+  /\bln\b/i,
+  /\btee\b/i,
+  /\btruncate\b/i,
+  /\bdd\b/i,
+  /(^|[^<])>(?!>)/,
+  />>/,
+  /\bnpm\s+(install|uninstall|update|ci|link|publish|version)\b/i,
+  /\byarn\s+(add|remove|install|publish|upgrade)\b/i,
+  /\bpnpm\s+(add|remove|install|publish|update)\b/i,
+  /\bbun\s+(add|remove|install|update|publish)\b/i,
+  /\bpip\s+(install|uninstall)\b/i,
+  /\buv\s+(add|remove|sync|lock|pip\s+install)\b/i,
+  /\bgit\s+(add|commit|push|pull|merge|rebase|reset|checkout|switch|stash|cherry-pick|revert|tag|init|clone)\b/i,
+  /\bsudo\b/i,
+  /\bsu\b/i,
+  /\bkill\b/i,
+  /\bpkill\b/i,
+  /\bkillall\b/i,
+  /\breboot\b/i,
+  /\bshutdown\b/i,
+  /\bsystemctl\s+(start|stop|restart|enable|disable)\b/i,
+  /\bservice\s+\S+\s+(start|stop|restart)\b/i,
+  /\b(vim?|nano|emacs|code|subl)\b/i,
+];
+
+export const SAFE_BASH_PATTERNS: RegExp[] = [
+  /^\s*(cat|head|tail|less|more|grep|find|ls|pwd|echo|printf|wc|sort|uniq|diff|file|stat|du|df|tree|which|whereis|type|env|printenv|uname|whoami|id|date|uptime|ps|jq|awk|rg|fd|bat|eza)\b/i,
+  /^\s*sed\s+-n\b/i,
+  /^\s*git\s+(status|log|diff|show|branch|remote|config\s+--get|ls-files|grep)\b/i,
+  /^\s*npm\s+(list|ls|view|info|search|outdated|audit)\b/i,
+  /^\s*(node|python|python3|npm|tsc|biome|ruff|ty)\s+--version\b/i,
+];
